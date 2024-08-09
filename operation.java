653f3536c832078019b1e0cb480c32d16d00b605
Ranadeb Saha <103212375+ranadebsaha@users.noreply.github.com>
2024-08-09 23:45:09 +0530

    Add files via upload

---
+++ operation.java
@@ -0,0 +1,18 @@
+class operation{
+    public static void main(String args[]){
+    int a=10;
+    int b=20;
+    int c;
+
+    c=a+b;
+    System.out.println("Addition is: "+c);
+    c=a-b;
+    System.out.println("Substration is: "+c);
+    c=a*b;
+    System.out.println("Multiplication is: "+c);
+    c=b/a;
+    System.out.println("Division is: "+c);
+    c=b%3;
+    System.out.println("Reminder is: "+c);
+    }
+}
