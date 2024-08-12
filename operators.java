046c45335fc0f0a495085f0f29441c65ce42a4b4
Ranadeb Saha <103212375+ranadebsaha@users.noreply.github.com>
2024-08-12 23:21:49 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,42 @@
+// Types of Operators in Java
+/* Unary Operator -->          expr++ expr-- ++expr --expr 
+Arithmetic Operator -->     + - * / %
+Shift Operator -->          << >>
+Relational Operator -->     < > == !=
+Bitwise Operator -->        & | 
+Logical Operator -->        && ||
+Ternary Operator -->        ? :
+Assignment Operator -->     = += -= *= /= %= */
+
+class operators{
+    public static void main(String args[]){
+        int a=10;
+        int b=20;
+        int c=15;
+
+        System.out.println(a*=20); //a=a*20
+
+        // System.out.println((a>b)?a:b);
+        // System.out.println(a<b | b<c);
+        // System.out.println(a<b || b<c);
+        
+        // System.out.println("a ->"+a);
+        // System.out.println(++a); 
+        // System.out.println("--a ->"+--a); 
+        // System.out.println("a ->"+a);
+
+        // System.out.println(a>>2); // 1010 -> 101000 ->10
+
+        // if(a!=b){
+        //     System.out.println("not same");
+        // }else{
+        //     System.out.println("both are same");
+        // }
+
+        
+            
+        
+        
+
+    }
+}
