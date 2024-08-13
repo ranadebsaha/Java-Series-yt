fb69bf9a22ab95bfb39d76a959beb9d5a6f01ff5
Ranadeb Saha <103212375+ranadebsaha@users.noreply.github.com>
2024-08-13 23:53:28 +0530

    Add files via upload

---
+++ oops.java
@@ -0,0 +1,42 @@
+class mobile{
+    int app=10;
+    int camera=5;
+    void print(){
+    System.out.println("In this mobile their are "+app+" app and "+camera+" camera");
+    }
+}
+class oops{
+    public static void main(String args[]){
+        // mobile m1;
+        // m1=new mobile();
+        // mobile m1=new mobile();
+        // m1.print();
+        new mobile().print();
+    }
+}
+// Class
+// A class in Java is a set of objects which shares 
+// common behavior/methods and common properties/attributes. 
+// It is a user-defined blueprint or prototype from which objects 
+// are created.
+
+
+// Object
+// An object in Java is a basic unit of Object-Oriented Programming 
+// and represents real-life entities. Objects are the instances of 
+// a class that are created to use the attributes and methods of a class.  A typical Java program creates many objects, which as you know, interact by invoking methods. An object consists of : 
+
+// State: It is represented by attributes/properties of an object. 
+
+// Behavior: It is represented by the methods of an object. 
+
+// Identity: It gives a unique name to an object and 
+//         enables one object to interact with other objects.
+
+
+// Anonymous object
+// In Java, an anonymous object is an object that is created 
+// without giving it a name.
+
+// An anonymous object is considered useful when you want to 
+// create an object not more than once in your program.
