06627075b3f6c5c6ce4a7fc87fea06d9e552df64
Ranadeb Saha <103212375+ranadebsaha@users.noreply.github.com>
2024-08-14 22:54:37 +0530

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,15 @@
+class ifelse{
+    public static void main(String args[]){
+        int a,b;
+        a=10;
+        b=10;
+        if(a>b){
+            System.out.println("a is Greater");
+        }else if(a==b){
+            System.out.println("Both numbers are same");
+        }
+        else{
+            System.out.println("b is Greater");
+        }
+    }
+}
